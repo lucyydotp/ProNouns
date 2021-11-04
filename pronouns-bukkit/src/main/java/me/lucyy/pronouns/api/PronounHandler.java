afd53aea01eeabd09c158d5205f233bb08d35f12
Lucy <lucy@poulton.xyz>
2021-11-04 17:57:17 +0000

    added stub interface for old PronounHandler to stop old versions of profiles from dying

---
+++ pronouns-bukkit/src/main/java/me/lucyy/pronouns/api/PronounHandler.java
@@ -0,0 +1,9 @@
+package me.lucyy.pronouns.api;
+
+/**
+ * This class is non-functional and is solely used to stop old versions of ProFiles
+ * from dying
+ */
+@Deprecated
+public interface PronounHandler {
+}
